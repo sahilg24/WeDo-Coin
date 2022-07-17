95e1bbc82900986fda2252fed7d544c36ddc3fcf
hanyuxie1234 <hanyuxie@brandeis.edu>
2022-07-17 13:28:03 -0700

    The login page

--- wedo-demo-page/src/pages/WedoAccountPage/WedoAccountPageDemo.js
+++ wedo-demo-page/src/pages/WedoAccountPage/WedoAccountPageDemo.js
@@ -1,8 +1,30 @@
+import PropTypes from 'prop-types';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Preferences from '../Preferences/Preferences';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+
+
+
 const WedoAccountPageDemo = () => {
 
     return (
-        <div>
-            <h1>This is account page</h1>
+        <div className="login-wrapper">
+            <h1>Please Log In</h1>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" />
+                 </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         </div>
     );
 };
