ad8af6ea05fcaa13a2cd27810437c7fd336f3c42
Duy Tran <60637571+deantran98@users.noreply.github.com>
2022-07-29 14:37:08 -0400

    added input fields for log in

--- wedo-demo-page/src/pages/WedoAccountPage/components/WedoAccountMenu.js
+++ wedo-demo-page/src/pages/WedoAccountPage/components/WedoAccountMenu.js
@@ -11,6 +11,8 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
 
 const theme = createTheme({
     palette: {
@@ -148,6 +150,96 @@ const WedoAccountMenu = () => {
                             >
                                 Log in to your Wedo.coin account
                             </Typography></div>
+                        <div style={{ width: '30rem', padding: '40px', marginLeft: 'auto', marginRight: 'auto' }}>
+                            <TextField
+                                fullWidth
+                                id="filled-textarea"
+                                label="Username"
+                                placeholder="Enter your username"
+                                multiline
+                                InputLabelProps={{
+                                    style: { color: "white" }
+                                }}
+                                InputProps={{
+                                    style: { color: "white" }
+                                }}
+                                variant="filled"
+                                sx={{
+                                    bgcolor: 'primary.main',
+                                }}
+                            /></div>
+
+                        <div style={{ width: '30rem', marginLeft: 'auto', marginRight: 'auto' }}>
+                            <TextField
+                                fullWidth
+                                id="filled-textarea"
+                                label="Password"
+                                placeholder="Enter your password"
+                                multiline
+                                InputLabelProps={{
+                                    style: { color: "white" }
+                                }}
+                                InputProps={{
+                                    style: { color: "white" }
+                                }}
+                                variant="filled"
+                                sx={{
+                                    bgcolor: 'primary.main',
+                                }}
+                            />
+                        </div>
+
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            pt={5}
+                        >
+                            <Button variant="contained"
+                            >Log in</Button></Box>
+
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            pt={2}
+                        >
+                            <Link href="#" underline="hover">
+                                {'Forgot password?'}
+                            </Link></Box>
+
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            pt={6}
+                        >
+                            <Typography
+                                variant="h5"
+                                sx={{
+                                    fontFamily: 'monospace',
+                                    fontWeight: 200,
+                                    color: 'primary.main',
+                                }}
+                            >
+                                Don't have an account yet?
+                            </Typography></Box>
+
+                        <Box
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                            pt={1}
+                        >
+                            <Link href="#" underline="hover"
+                                variant="h5"
+                                sx={{
+                                    fontFamily: 'monospace',
+                                    fontWeight: 'bold',
+                                    color: 'primary.main',
+                                }}>
+                                {'Click here to start trading.'}
+                            </Link></Box>
                     </Box>
 
                 </Grid>
